e48aacc001f7d243309af0f410bab374d71288cc
Alin Migea <alinmigea@gmail.com>
2015-12-15 00:20:42 +0200

    Add empty Trains project

---
+++ JuniorMind/ProblemTrains/TrainsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemTrains
+{
+    [TestClass]
+    public class TrainsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
