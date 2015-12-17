8a06ec7a450f8d5a0b8accfd50781828a7bb0d29
Alin Migea <alinmigea@gmail.com>
2015-12-17 23:56:21 +0200

    Trains project: version 1
    Added tests for the method that calculates the distance traveled.

--- JuniorMind/ProblemTrains/TrainsTests.cs
+++ JuniorMind/ProblemTrains/TrainsTests.cs
@@ -7,8 +7,48 @@ namespace ProblemTrains
     public class TrainsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DistanceDone()
         {
+            float distance = CalculateDistanceTraveled(0);
+            Assert.AreEqual(0 , distance);
+        }
+
+        [TestMethod]
+        public void DistanceDone2()
+        {
+            float distance = CalculateDistanceTraveled(1);
+            Assert.AreEqual(2, distance);
+        }
+
+        [TestMethod]
+        public void DistanceDone3()
+        {
+            float distance = CalculateDistanceTraveled(2);
+            Assert.AreEqual(4, distance);
+        }
+
+        [TestMethod]
+        public void DistanceDone4()
+        {
+            float distance = CalculateDistanceTraveled(10);
+            Assert.AreEqual(20, distance);
+        }
+
+        [TestMethod]
+        public void DistanceDone5()
+        {
+            float distance = CalculateDistanceTraveled(1005);
+            Assert.AreEqual(2010, distance);
+        }
+
+        float CalculateDistanceTraveled(int DistanceTraveled)
+        {
+            int TotalDistance = 4 * DistanceTraveled;
+            float DistanceNeedsTraveling;
+
+            DistanceNeedsTraveling = (float) 0.5 * TotalDistance;
+
+            return DistanceNeedsTraveling;
         }
     }
 }
