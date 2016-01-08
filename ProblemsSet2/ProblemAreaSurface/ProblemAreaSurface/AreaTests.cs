7f94c3b7dd932ee22b69d31276c2cde2e3903694
Alin Migea <alinmigea@gmail.com>
2016-01-08 17:39:25 +0200

    Problem Area Surface
    First commit for the Problem. Calculates the positive result of a second
    grade equation.

---
+++ ProblemsSet2/ProblemAreaSurface/ProblemAreaSurface/AreaTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemAreaSurface
+{
+    [TestClass]
+    public class AreaTests
+    {
+        [TestMethod]
+        public void TestInitialArea()
+        {
+            double area = CalculateAreaOfRectangle();
+            Assert.AreEqual(770, area);
+        }
+
+        double CalculateAreaOfRectangle()
+        {
+            //initial masurements of the Area
+            double totalArea = 770000;
+            double widthArea = 230;
+
+            //formula for that equation x^2 + 230*x - totalArea = 0
+            double delta = Math.Pow(widthArea, 2) + 4 * totalArea;
+            double positiveArea = (Math.Sqrt(delta) - widthArea) / 2;
+            return Math.Truncate(positiveArea);
+        }
+    }
+}
