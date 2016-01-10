de1f718b1608a9b3b150391a14ad3e2455c9962f
Alin Migea <alinmigea@gmail.com>
2016-01-11 01:15:35 +0200

    Second Commit - Problem Area Surface
    I've generalized the function's input and add another tests.

--- ProblemsSet2/ProblemAreaSurface/ProblemAreaSurface/AreaTests.cs
+++ ProblemsSet2/ProblemAreaSurface/ProblemAreaSurface/AreaTests.cs
@@ -7,21 +7,33 @@ namespace ProblemAreaSurface
     public class AreaTests
     {
         [TestMethod]
-        public void TestInitialArea()
+        public void ZeroBoughtZeroInitial()
         {
-            double area = CalculateAreaOfRectangle();
+            double area = CalculateAreaOfRectangle(0, 0);
+            Assert.AreEqual(0, 0);
+        }
+
+        [TestMethod]
+        public void OneHundredBoughtOneThousandInitial()
+        {
+            double area = CalculateAreaOfRectangle(100, 1000);
+            Assert.AreEqual(9, area);
+        }
+
+
+        [TestMethod]
+        public void TwoHundredThirtyBoughtSevenHundredThousandInitial()
+        {
+            double area = CalculateAreaOfRectangle(230, 770000);
             Assert.AreEqual(770, area);
         }
 
-        double CalculateAreaOfRectangle()
+        double CalculateAreaOfRectangle(double widthArea, double totalArea)
         {
-            //initial masurements of the Area
-            double totalArea = 770000;
-            double widthArea = 230;
-
-            //formula for that equation x^2 + 230*x - totalArea = 0
+            //formula for that equation x^2 + widthArea*x - totalArea = 0
             double delta = Math.Pow(widthArea, 2) + 4 * totalArea;
             double positiveArea = (Math.Sqrt(delta) - widthArea) / 2;
+            //gives a result with aproximation
             return Math.Truncate(positiveArea);
         }
     }
